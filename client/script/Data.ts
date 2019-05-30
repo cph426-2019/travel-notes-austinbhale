a42d3f54bc6ebe49644661f3fa4a1536493d9806
austinbhale <haleau@live.unc.edu>
2019-05-30 21:43:21 +0200

    Server-side rendering working!

--- client/script/Data.ts
+++ client/script/Data.ts
@@ -1,42 +1,42 @@
 export default [
     {
-        "thumbSrc": "./img/aros-art-thumbnail.jpg",
-        "src": "./img/aros-art.jpg",
+        "thumbSrc": "img/aros-art-thumbnail.jpg",
+        "src": "img/aros-art.jpg",
         "caption": "Aros Interactive Art Exhibit",
     },
     {
-        "thumbSrc": "./img/aros-blue-thumbnail.jpg",
-        "src": "./img/aros-blue.jpg",
+        "thumbSrc": "img/aros-blue-thumbnail.jpg",
+        "src": "img/aros-blue.jpg",
         "caption": "Aros Blue Panorama",
     },
     {
-        "thumbSrc": "./img/aros-purple-thumbnail.jpg",
-        "src": "./img/aros-purple.jpg",
+        "thumbSrc": "img/aros-purple-thumbnail.jpg",
+        "src": "img/aros-purple.jpg",
         "caption": "Aros Purple Panorama",
     },
     {
-        "thumbSrc": "./img/church-thumbnail.jpg",
-        "src": "./img/church.jpg",
+        "thumbSrc": "img/church-thumbnail.jpg",
+        "src": "img/church.jpg",
         "caption": "Church by Round Tower",
     },
     {
-        "thumbSrc": "./img/aros-yellow-thumbnail.jpg",
-        "src": "./img/aros-yellow.jpg",
+        "thumbSrc": "img/aros-yellow-thumbnail.jpg",
+        "src": "img/aros-yellow.jpg",
         "caption": "Aros Yellow Panorama",
     },
     {
-        "thumbSrc": "./img/freetown-thumbnail.jpg",
-        "src": "./img/freetown.jpg",
+        "thumbSrc": "img/freetown-thumbnail.jpg",
+        "src": "img/freetown.jpg",
         "caption": "Freetown Christiana",
     },
     {
-        "thumbSrc": "./img/slow-magic-thumbnail.jpg",
-        "src": "./img/slow-magic.jpg",
+        "thumbSrc": "img/slow-magic-thumbnail.jpg",
+        "src": "img/slow-magic.jpg",
         "caption": "Slow Magic Concert",
     },
     {
-        "thumbSrc": "./img/sunset-thumbnail.jpg",
-        "src": "./img/sunset.jpg",
+        "thumbSrc": "img/sunset-thumbnail.jpg",
+        "src": "img/sunset.jpg",
         "caption": "Sunset at Copenhagen",
     },
 ];
--- server/index.ts
+++ server/index.ts
@@ -13,7 +13,31 @@ app.engine("hbs", exphbs({
 app.use(express.static("dist/"));
 
 app.get("/", (req, res) => {
-    res.render("index");
+    res.render("index", {
+        title: "Austin's Notes",
+        sectionHdr: "index-hdr",
+    });
+});
+
+app.get("/about-me", (req, res) => {
+    res.render("about-me", {
+        title: "About Me",
+        sectionHdr: "about-me-hdr",
+    });
+});
+
+app.get("/bucket-list", (req, res) => {
+    res.render("bucket-list", {
+        title: "Bucket List",
+        sectionHdr: "bucket-list-hdr",
+    });
+});
+
+app.get("/gallery", (req, res) => {
+    res.render("gallery", {
+        title: "Photo Gallery",
+        sectionHdr: "gallery-hdr",
+    });
 });
 
 app.listen(1234, () => console.log("Listening on 1234"))
